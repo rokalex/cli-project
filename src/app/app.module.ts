f8f327239ce2a0c70be3bfc5c99e6ce9f1864715
Alexandre Rocca <alexandrerocca@MacBook-Pro-de-Alexandre.local>
2017-06-14 13:00:11 +0200

    wip with materializecss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { MaterializeModule } from 'angular2-materialize';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
